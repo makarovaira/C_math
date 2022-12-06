7a0199cf234a7af3bda440a19d2c08836da43f4d
Winford Tracy <winfordt@un-j2.kzn.21-school.ru>
2022-12-06 23:32:53 +0300

    upt

--- src/s21_math.c
+++ src/s21_math.c
@@ -3,13 +3,11 @@
 #include "s21_math.h"
 
 int s21_abs(int x) {
-    x = (x < 0) ? x * (-1) : x;
-    return x;
+    return x < 0 ? -x : x;
 }
 
 long double s21_fabs(double x) {
-    x = (x < 0) ? x * (-1) : x;
-    return x;
+    return x < 0 ? -x : x;
 }
 
 int s21_factorial (int n) {
@@ -69,28 +67,6 @@ long double s21_tan(double x) {
     return tang;
 }
 
-// long double s21_atan(double x) {
-//     double result = x;
-//     double elem = x;
-//     double ch = x;
-//     double zn = 1.0;
-//     for (int i = 1; i <= 100; i++) {
-//             ch = ch * x * x;
-//             zn = 2.0 * i + 1;
-//             //printf("%lf\n", ch);
-//             // printf("%lf\n", elem * ch);
-//             elem = ch/zn;
-//         if (i % 2 == 0)
-//             result += elem;
-//         else
-//             result -= elem;
-//     }
-    // if (x >= 1 && x <= -1) {
-    //     result = s21_PI / 2 - result;
-    // }
-    // return result;
-// }
-
 long double s21_atan(double x) {
     long double res = x;
     long double ch = x;
@@ -127,7 +103,6 @@ long double s21_atan(double x) {
 }
 
 long double s21_sqrt(double x) {
-    long double ans;
     long double s1 = 0, s2 = 4;
     while (s21_fabs(s2 - s1) > 1e-10) {
         if (x < 0) {
@@ -140,7 +115,7 @@ long double s21_sqrt(double x) {
 }
 
 long double s21_asin(double x) {
-    long double result;
+    long double result = 0;
     int sign = 1;
     if (x < 0) {
         sign = -1;
@@ -157,40 +132,35 @@ long double s21_asin(double x) {
 }
 
 long double s21_acos(double x) {
-    long double result;
-    if (x <= 1 && x >= -1) {
-        result = s21_PI / 2 - s21_asin(x);
-    }
-    return result;
+    return (x <= 1 && x >= -1) ? s21_PI / 2 - s21_asin(x) : 0.;
 }
 
 long double s21_log(double x) {
     return (x > 1) ? 1 + s21_log(x / 5) : 0;
 }
 
-// long double s21_pow(double x, double y) {
-    
-// }
-
 long double s21_exp(double x) {
-    long double result = 1;
-    long double ch = x;
-    long double zn = 1;
-    long double elem = x;
-    int n = 1;
-    while (s21_fabs((double)elem) > s21_EPS) {
-        ch = ch * x;
-        zn = zn * n;
-        elem = ch / zn;
-        result += elem;
-        n += 1;
+    long double res = 1, y = 1, n = 1;
+    for (; s21_fabs((double)res) > s21_EPS; y+=res) {
+        if (y > s21_DBL_MAX || y < s21_DBL_MIN) {
+            y = (y > s21_DBL_MAX) ? s21_inf : 0.;
+            break;
+        }
+        res *= x/n++;
     }
-    return result;
+    return y;
 }
 
+long double s21_fmod(double x, double y) {
+    long double rem = s21_fabs(x);
+    for(int i = 1; rem > s21_fabs(y); i++) {
+        rem = s21_fabs(x) - y * i;
+    }
+    return x < 0 ? -rem : rem;
+}
 
-void main() {
-    printf("kon = %lf\n", exp(12));
-    printf("u nas = %Lf\n", s21_exp(12));
+int main() {
+    printf("kon = %lf\n", fmod(-234.432, 23));
+    printf("u nas = %Lf\n", s21_fmod(-234.432,23));
 }
 
